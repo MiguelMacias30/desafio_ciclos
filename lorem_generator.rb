f5c8defac817e07989dedc6479d33cf9260f9ea9
Miguel Macias <miguelfernandomacias@gmail.com>
2020-08-22 23:18:22 -0400

    lorem

---
+++ lorem_generator.rb
@@ -0,0 +1,9 @@
+n = ARGV[0].to_i
+n.times do |i|
+  puts"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin leo et accumsan semper.
+  Fusce convallis iaculis lectus sit amet commodo. Morbi lorem arcu, tristique lobortis commodo vel, dictum vel leo.
+  Phasellus convallis condimentum massa, egestas ullamcorper erat aliquet non. Quisque volutpat venenatis ipsum,
+  eu rhoncus nunc lobortis et. Proin pharetra congue libero non posuere. Fusce pellentesque finibus aliquam.
+  Maecenas placerat arcu et tristique lobortis"
+  puts ''
+end
